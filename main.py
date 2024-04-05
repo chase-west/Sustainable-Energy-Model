5c57144588757500e8937f62a74e92a73843a385
chase-west <chasetwest@gmail.com>
2024-04-05 11:42:40 -0700

    cleaned up packages, removed mongo

--- main.py
+++ main.py
@@ -2,25 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
-import os 
-
-load_dotenv()
-password = os.getenv("MONGO_PASSWORD")
-
-uri = f"mongodb+srv://chasetwest:{password}@cluster0.k7it9kr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 #load the data
 data = pd.read_csv('data.csv')

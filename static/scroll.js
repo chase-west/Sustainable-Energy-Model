d2125f78924c1dac6edf3af8fb41eed543fe6ede
chase-west <chasetwest@gmail.com>
2024-10-22 20:23:01 -0700

    Added some transitions

--- static/scroll.js
+++ static/scroll.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const scroll = new LocomotiveScroll({
-      el: document.querySelector('[data-scroll-container]'),
-      smooth: true,
-  });
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
 });

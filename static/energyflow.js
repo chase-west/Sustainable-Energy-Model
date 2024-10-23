b9b2d31df13e75cb860642f5ae69688ea0d1cf82
chase-west <chasetwest@gmail.com>
2024-10-22 18:40:29 -0700

    Removed scrolling animation

--- static/energyflow.js
+++ static/energyflow.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('resize', updateEnergyFlow);
+    updateEnergyFlow();
+});
+
 const energyFlow = document.querySelector('.energy-flow');
 const energyPath = document.querySelector('.energy-path');
 const energyGlow = document.querySelector('.energy-glow');
@@ -7,7 +12,7 @@ function updateEnergyFlow() {
     const width = window.innerWidth;
 
     energyFlow.setAttribute('viewBox', `0 0 ${width} ${height}`);
-    const curve = `M0,${height/2} Q${width/4},${height/2-170} ${width/2},${height/2} T${width},${height/2}`;
+    const curve = `M0,${height / 2} Q${width / 4},${height / 2 - 170} ${width / 2},${height / 2} T${width},${height / 2}`;
     energyPath.setAttribute('d', curve);
     energyGlow.setAttribute('d', curve);
 }

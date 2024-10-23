da036b22d9739c659b131ce82c9c2cf6e980b2c9
chase-west <chasetwest@gmail.com>
2024-10-22 19:58:47 -0700

    Fixed smooth scroll

---
+++ static/scroll.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+  });
+});
